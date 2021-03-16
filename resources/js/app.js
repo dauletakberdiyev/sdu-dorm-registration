f841476a16db9022c2a7e618b49e5e1bd74fae39
Daulet Akberdiyev <170107034@stu.sdu.edu.kz>
2021-03-16 15:53:02 +0600

    fronted was added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import Vue from 'vue';
+import Axios from 'axios';
+import front from './components/Front'
+
+Vue.prototype.$http = Axios;
+
+const app = new Vue({
+    render:h => h(front),
+}).$mount('#app');
